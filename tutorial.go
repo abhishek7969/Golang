b434c58a0beed2e2985bce66fdb87b52fd37e901
Abhishek kumar <abhishek7969@gmail.com>
2020-05-20 21:14:13 +0530

    Add files via upload

---
+++ tutorial.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var temp
+	var abhi string = "Hello Abhishek Kumar !"
+	//temp = "Rathur"
+
+	var num uint16 = 260
+	var num1 = 260.98
+	num2 := 6
+	var num3 uint64
+	var b1 bool
+	//flag := true
+
+	fmt.Println("Hi", abhi, num+5, num3, b1)
+	fmt.Printf("%T", abhi)
+	fmt.Printf("%T", num1)
+	fmt.Printf("%T", num2)
+
+}
